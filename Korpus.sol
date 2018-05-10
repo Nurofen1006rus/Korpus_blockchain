1e782869cfce4e6fea1128d516a1861f1f1ce666
Nikita <nurofen38964@gmail.com>
2018-05-10 18:43:16 +0500

    Create Korpus.sol

---
+++ Korpus.sol
@@ -0,0 +1,151 @@
+pragma solidity 0.4.23;
+
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+contract ERC20Basic {
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+contract Ownable {
+  address public owner;
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+  constructor() public {
+    owner = msg.sender;
+  }
+  
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    emit OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+}
+contract KorpToken is Ownable, ERC20{
+    using SafeMath for uint256;
+    
+    mapping(address => uint256) public balances;
+    mapping(address => mapping (address => uint256)) public allowed;
+    
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 totalSupply_;
+    
+    constructor(string _name, string _symbol, uint8 _decimals, uint256 _totalSupply) public {
+        owner = msg.sender;
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply_ = _totalSupply;
+        
+        balances[owner] = _totalSupply;
+        emit Transfer(0x0, owner, _totalSupply);
+    }
+    
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
+    
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+    function approve(address _spender, uint256 _value) public returns (bool) {
+    allowed[msg.sender][_spender] = _value;
+    emit Approval(msg.sender, _spender, _value);
+    return true;
+  }
+    function allowance(address _owner, address _spender) public view returns (uint256) {
+        return allowed[_owner][_spender];
+    }
+    function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+    function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+        uint oldValue = allowed[msg.sender][_spender];
+        if (_subtractedValue > oldValue) {
+            allowed[msg.sender][_spender] = 0;
+        } else {
+            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+        }
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+}
+contract TokenSale{
+    using SafeMath for uint256;
+    
+    ERC20 public token;
+    address public wallet;
+    uint256 public rate;
+    uint256 public weiRaised;
+    
+    event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+    
+    constructor(uint256 _rate, address _wallet, ERC20 _token) public {
+        require(_rate > 0);
+        require(_wallet != address(0));
+        require(_token != address(0));
+    
+        rate = _rate;
+        wallet = _wallet;
+        token = _token;
+    }
+    
+    function () external payable {
+        buyTokens(msg.sender);
+    }
+
+    function buyTokens(address _beneficiary) public payable {
+        uint256 weiAmount = msg.value;
+        require(_beneficiary != address(0));
+        require(weiAmount != 0);
+        uint256 tokensAmount = weiAmount.mul(rate);
+        weiRaised = weiRaised.add(weiAmount);
+        token.transfer(_beneficiary, tokensAmount);
+        wallet.transfer(msg.value);
+        emit TokenPurchase(msg.sender, _beneficiary, weiAmount, tokensAmount);
+  }
+}
